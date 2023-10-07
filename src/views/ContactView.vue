396c35190e9fa296ce4b3e83bfd450cff9e26217
artemsites <info@artemsites.ru>
2023-10-07 10:19:09 +0400

    шапку контакта и её адаптацию

---
+++ src/views/ContactView.vue
@@ -0,0 +1,66 @@
+<template>
+  <HeaderComponent class="contact__header">
+    <AvatarContact class="contact__avatar" name="Двери Вадим" />
+    Двери Вадим
+
+    <button class="contact__close">
+      <IconX />
+    </button>
+  </HeaderComponent>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import HeaderComponent from "../components/HeaderComponent.vue";
+import AvatarContact from "../components/AvatarContact.vue";
+import IconX from "../components/icons/IconX.vue";
+
+export default defineComponent({
+  name: "ContactView",
+
+  components: {
+    HeaderComponent,
+    AvatarContact,
+    IconX,
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.contact {
+  &__header {
+    justify-content: center;
+  }
+
+  &__avatar {
+    width: 1.25rem;
+    height: 1.25rem;
+
+    font-size: 0.625rem;
+    line-height: 0;
+
+    margin-right: 0.5rem;
+
+    @media screen and (min-width: 992px) {
+      width: 1.5rem;
+      height: 1.5rem;
+
+      font-size: 0.75rem;
+    }
+  }
+
+  &__close {
+    width: 1.5rem;
+    height: 1.5rem;
+
+    position: absolute;
+    right: 0.75rem;
+    @media screen and (min-width: 992px) {
+      width: 2rem;
+      height: 2rem;
+    }
+  }
+}
+</style>
+
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -92,7 +92,6 @@ export default defineComponent({
       })
     }
   },
-
 })
 </script>
 
